f12863d892c075064bf52b010587e8405ee1350b
Ahmed Shband <60365776+Ahmed404-404@users.noreply.github.com>
2023-04-30 23:42:55 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+let container = document.querySelector(".container"),
+    navBtns = document.querySelectorAll('i'),
+    cardWidth = document.querySelector(".card").clientWidth,
+    isDragging = false,
+    prevScroll,
+    prevMouse,
+    possdiff;
+
+
+navBtns.forEach(ele=>{
+    ele.addEventListener("click",()=>{
+        container.scrollLeft += ele.classList.contains("right")? cardWidth - 13 : -cardWidth - 13
+    });
+});
+
+
+const dragStart = (e)=>{
+    isDragging = true;;
+    prevMouse = e.clientX || e.touches[0].pageX;
+    prevScroll = container.scrollLeft;
+    
+};
+
+const dragging = (e)=>{
+    if(!isDragging) return;
+    container.classList.add("dragging");
+    possdiff = (e.clientX || e.touches[0].pageX) - prevMouse ;
+    container.scrollLeft = prevScroll + -possdiff;
+};
+
+const dragStop = (e)=>{
+    container.classList.remove("dragging");
+    isDragging = false;
+};
+
+
+container.addEventListener("mousedown",dragStart);
+container.addEventListener("touchstart",dragStart);
+
+container.addEventListener("mousemove",dragging);
+container.addEventListener("touchmove",dragging);
+
+container.addEventListener("mouseup",dragStop);
+container.addEventListener("mouseleave",dragStop);
+container.addEventListener("touchend",dragStop);
+
